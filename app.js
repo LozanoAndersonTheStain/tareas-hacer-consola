8f0d4d84f684fed8f511553093647eb6012a25e7
LozanoTheStain <adslozanov@gmail.com>
2022-10-06 20:49:51 -0500

    Se realizo la accion de crear y almacenar la tarea

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const colors = require('colors')
-const { inquirerMenu, pausa } = require('./helpers/inquirer')
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer')
+const Tareas = require('./models/tareas')
+const { guardarDB, leerDB } = require('./helpers/guardarLeerArchivo')
 
 const main = async () => {
   let opt = ''
+  const tareas = new Tareas()
+
+  const tareasDB = leerDB()
+  if (tareasDB) {
+    tareas.cargarTareasFromArra(tareasDB)
+  }
 
   do {
     opt = await inquirerMenu()
-    console.log(opt)
+
+    switch (opt) {
+      case 1:
+        const desc = await leerInput('Descripcion:')
+        tareas.crearTarea(desc)
+        break
+      case 2:
+        console.log(tareas.getListadoArra)
+        break
+    }
+
+    guardarDB(tareas.getListadoArra)
     if (opt !== '0') await pausa()
   } while (opt !== '0')
 }
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,50 +1,50 @@
 const inquirer = require('inquirer')
 const colors = require('colors')
 
-const preguntas = [
-  {
-    type: 'list',
-    name: 'opcion',
-    message: '¿Que deseas hacer?',
-    choices: [
-      {
-        value: 1,
-        name: `${'1.'.grey} Crear tarea`,
-      },
-      {
-        value: 2,
-        name: `${'2.'.grey} Listar tareas`,
-      },
-      {
-        value: 3,
-        name: `${'3.'.grey} Listar tareas completadas`,
-      },
-      {
-        value: 4,
-        name: `${'4.'.grey} Listar tareas pendientes`,
-      },
-      {
-        value: 5,
-        name: `${'5.'.grey} Completar tarea(s)`,
-      },
-      {
-        value: 6,
-        name: `${'6.'.grey} Borrar tareas`,
-      },
-      {
-        value: 7,
-        name: `${'7.'.grey} Salir`,
-      },
-    ],
-  },
-]
-
 const inquirerMenu = async () => {
   console.clear()
 
-  console.log('===================='.blue)
-  console.log('Seleccione una opción'.cyan)
-  console.log('===================='.blue)
+  console.log('======================='.blue)
+  console.log(' Seleccione una opción'.cyan)
+  console.log('======================='.blue)
+
+  const preguntas = [
+    {
+      type: 'list',
+      name: 'opcion',
+      message: '¿Que deseas hacer?',
+      choices: [
+        {
+          value: 1,
+          name: `${'1.'.grey} Crear tarea`,
+        },
+        {
+          value: 2,
+          name: `${'2.'.grey} Listar tareas`,
+        },
+        {
+          value: 3,
+          name: `${'3.'.grey} Listar tareas completadas`,
+        },
+        {
+          value: 4,
+          name: `${'4.'.grey} Listar tareas pendientes`,
+        },
+        {
+          value: 5,
+          name: `${'5.'.grey} Completar tarea(s)`,
+        },
+        {
+          value: 6,
+          name: `${'6.'.grey} Borrar tareas`,
+        },
+        {
+          value: 7,
+          name: `${'7.'.grey} Salir`,
+        },
+      ],
+    },
+  ]
 
   const { opcion } = await inquirer.prompt(preguntas)
   return opcion
@@ -58,11 +58,32 @@ const pausa = async () => {
       message: `Presione ${'enter'.blue} para continuar`,
     },
   ]
+
   console.log('\n')
   await inquirer.prompt(question)
+}
+
+const leerInput = async (message) => {
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message,
+      valide(value) {
+        if (value.length === 0) {
+          return 'Por favor ingrese una opción'
+        }
+        return true
+      },
+    },
+  ]
+
+  const { desc } = await inquirer.prompt(question)
+  return desc
 }
 
 module.exports = {
   inquirerMenu,
   pausa,
+  leerInput,
 }
